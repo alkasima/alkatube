b4bdc976d849863f69dc8d92150bd4ca562d7c53
Kasim Abubakar Jajere <alkasima@gmail.com>
2023-07-13 09:21:08 +0100

    Fixing Allroute spacing

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -5,9 +5,8 @@ export const AllRoutes = () => {
     return (
 
         <div className="dark:bg-slate-800">
+           
             <Routes>
-
-                //Movies Routes
                 <Route path="" element={<MovieList apiPath="movie/now_playing" title="Home" />} />
                 <Route path="movie/:id" element={<MovieDetail />} />
                 <Route path="movies/popular" element={<MovieList apiPath="movie/popular" title="Popular" />} />
@@ -15,11 +14,8 @@ export const AllRoutes = () => {
                 <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming"/>} />
                 <Route path="search" element={<Search apiPath="search/movie" />} />
                 <Route path="*" element={<NotFound title="Page Not Found" />} />
-                
-                //Celebrities Routes
                 <Route path="celebrities" element={<CelebrityList title="Celebrities" />} />
                 <Route path="celebrity/:id" element={<CelebrityDetails />} />
-            
             </Routes>
         </div>
     )
